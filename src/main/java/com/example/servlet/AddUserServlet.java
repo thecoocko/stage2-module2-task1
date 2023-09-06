ef13be4f54b5f5333316618522f91d69ded23b3e
kakuracha <krabik4794@gmail.com>
2023-09-06 12:40:19 +0200

    done

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,6 +1,46 @@
 package com.example.servlet;
 
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (Objects.nonNull(request)) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/users.jsp");
+            if (Objects.nonNull(dispatcher)) {
+                dispatcher.forward(request, response);
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        if(Objects.nonNull(request)){
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            if(firstName!=null && lastName!=null){
+                User user = new User();
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                request.setAttribute("user",user);
+                Warehouse.getInstance().addUser(user);
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/add.jsp");
+            if(Objects.nonNull(dispatcher)){
+                dispatcher.forward(request,response);
+            }
+        }
+    }
 }
